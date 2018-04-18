2120dca48936b82e5caa94ce7933a9274dd0bf0f
Garg <natwar.garg@capgemini.com>
2018-04-18 16:59:31 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * redux example - fire action (increment and decrement ) to update state
+ */
+
+ // create reducer function - return new state always 
+
+ // create action (with event)
+
+ // create initial state 
+
+ 
+
